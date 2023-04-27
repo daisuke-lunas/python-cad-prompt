9944358e3c43d1d3e8e8e1ee7854c742d54d0c0f
daisuke-lunas <tamura.lunas@gmail.com>
2023-04-27 23:09:42 +0900

    綺麗に外壁が厚めに描写できるところまで

--- main/main.py
+++ main/main.py
@@ -6,6 +6,30 @@ from settings import FONT_NAME
 LAYER_NAME = "layer1"
 TEXT_LAYER = "textLayer"
 
+
+def drawWalls() -> None:
+    left = 0
+    top = 30
+    right = 40
+    bottom = 0
+
+    # 上の外壁
+    wall1 = items.Wall(left - 1, top+1, right+2)
+    wall1.draw(msp, LAYER_NAME)
+
+    # 左の外壁
+    wall2 = items.Wall(left - 1, top+1, top+2, isVertical=True)
+    wall2.draw(msp, LAYER_NAME)
+
+    # 右の外壁
+    wall3 = items.Wall(right, top+1, top+2, isVertical=True)
+    wall3.draw(msp, LAYER_NAME)
+
+    # 下の外壁
+    wall4 = items.Wall(left - 1, bottom, right+2)
+    wall4.draw(msp, LAYER_NAME)
+
+
 # dxfのversion指定
 doc = ezdxf.new(ezdxf.const.DXF2018, setup=True)
 # 日本語フォントの設定(.ttfのみ使用可能)
@@ -16,12 +40,14 @@ msp = doc.modelspace()
 doc.layers.new(name=LAYER_NAME, dxfattribs={'lineweight': '20'})
 doc.layers.new(name=TEXT_LAYER, dxfattribs={})
 
-room = items.Room(0, 0, 15, 10, "最初の部屋")
+room = items.Room(0, 10, 15, 10, "最初の部屋")
 room.draw(msp, LAYER_NAME)
+drawWalls()
+
 
 # 保存
 path = 'square'
 doc.saveas(path+'.dxf')
 
 # 画像で保存
-matplotlib.qsave(msp, path + '.png', size_inches=[1.6, 0.9])
+matplotlib.qsave(msp, path + '.png', size_inches=[2.4, 1.35])
--- main/model/items.py
+++ main/model/items.py
@@ -6,14 +6,14 @@ from settings import FONT_NAME
 class Room:
     # 線で囲われた四角いもの
     # 始点と幅/高さを持つ
-    left: int
-    top: int
-    height: int
-    width: int
+    left: float
+    top: float
+    height: float
+    width: float
     name: str
     # 名前を持つので、真ん中？に文字列を表記できるように
 
-    def __init__(self, left: int, top: int, height: int, width: int, name: str) -> None:
+    def __init__(self, left: float, top: float, width: float, height: float, name: str) -> None:
         self.left = left
         self.top = top
         self.height = height
@@ -23,8 +23,8 @@ class Room:
     def draw(self, msp: Modelspace, layer: str) -> None:
         points = [(self.left, self.top),
                   (self.left + self.width, self.top),
-                  (self.left + self.width, self.top + self.height),
-                  (self.left, self.top + self.height)]
+                  (self.left + self.width, self.top - self.height),
+                  (self.left, self.top - self.height)]
         ply = msp.add_polyline2d(points=points,
                                  format="xy",
                                  close=True,
@@ -33,40 +33,95 @@ class Room:
                                              'linetype': "Continuous",
                                              })
         hatch = msp.add_hatch(color=3)
-        hatch.set_pattern_fill("GOST_WOOD", scale=self.width/10)
+        hatch.set_pattern_fill("ANSI34", scale=0.3)
         hatch.paths.add_polyline_path(
             path_vertices=ply.points(),
             is_closed=True
         )
         msp.add_text(text=self.name,
-                     height=self.height/10,
+                     height=self.height/20,
                      dxfattribs={'layer': layer, 'style': FONT_NAME}).set_placement(
-            ((self.left + self.width)/2, (self.top + self.height)/2),
+            ((self.left + self.width)/2, self.top - self.height/2),
             align=TextEntityAlignment.CENTER
         )
 
 
 class Wall:
     # こちらも線で囲われた四角いもの
-    left: int
-    top: int
+    left: float
+    top: float
     # 構造は部屋と同じ。ただ、おきたい位置に対しちょっと外が始点(それは使う側で対処)
-    length: int  # 壁は長さ。縦横どちらにも使うので。
+    length: float  # 壁は長さ。縦横どちらにも使うので。
+    isVertical: bool  # 縦か横か
 
-    # 暑い壁と薄い壁が存在する（固定値でいいと思う）
-    _thinWidth = 5
-    _thickWidth = 10
+    # ドアの位置と幅
+    doorStartPos: float
+    doorLength: float
 
-    def __init__(self, left, top, length) -> None:
+    # 厚い壁と薄い壁が存在する（固定値）
+    isThin: bool
+    _thinWidth = 0.5
+    _thickWidth = 1
+
+    def __init__(self, left: float, top: float, length: float,
+                 doorStartPos=0.0, doorLength=0.0, isVertical=False, isThin=False) -> None:
         self.left = left
         self.top = top
         self.length = length
+        self.doorStartPos = doorStartPos
+        self.doorLength = doorLength
+        self.isVertical = isVertical
+        self.isThin = isThin
+
+    def draw(self, msp: Modelspace, layer: str) -> None:
+        width = self._thinWidth if self.isThin else self._thickWidth
+
+        rightX = self.left + width if self.isVertical else self.left + self.length
+        bottomY = self.top - self.length if self.isVertical else self.top - width
+
+        points = [(self.left, self.top), (rightX, self.top),
+                  (rightX, bottomY), (self.left, bottomY)]
+        ply = msp.add_polyline2d(points=points,
+                                 format="xy",
+                                 close=True,
+                                 dxfattribs={'layer': layer,
+                                             'color': 7,
+                                             'linetype': "Continuous",
+                                             })
+        hatch = msp.add_hatch(color=3)
+        hatch.set_pattern_fill("ANSI37", scale=0.3)
+        hatch.paths.add_polyline_path(
+            path_vertices=ply.points(),
+            is_closed=True
+        )
+
+        if self.doorLength > 0:
+            # ドアを描く
+            doorLeftX = self.left if self.isVertical else self.left + self.doorStartPos
+            doorTopY = self.top + self.doorStartPos if self.isVertical else self.top
+            doorRightX = self.left + width if self.isVertical else doorLeftX + self.doorLength
+            doorBottomY = doorTopY - self.doorLength if self.isVertical else self.top - width
+
+            doorPoints = [(doorLeftX, doorTopY), (doorRightX, doorTopY),
+                          (doorRightX, doorBottomY), (doorLeftX, doorBottomY)]
+            ply2 = msp.add_polyline2d(points=doorPoints,
+                                      format="xy",
+                                      close=True,
+                                      dxfattribs={'layer': layer,
+                                                  'color': 4,
+                                                  'linetype': "Continuous",
+                                                  })
+            hatch2 = msp.add_hatch(color=9)
+            hatch2.paths.add_polyline_path(
+                path_vertices=ply2.points(),
+                is_closed=True
+            )
 
 
 class Pillar:
-    x: int
-    y: int
-    radius: int
+    x: float
+    y: float
+    radius: float
     # 点と、太さ（ピクセル指定）を持つ
     # その点を中心に、正方形に広がる
 
